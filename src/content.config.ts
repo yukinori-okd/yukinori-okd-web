d643380f66ddb3ca80d32906cf757f81deed7896
Yukinori Okada <yukinori.okd@gmail.com>
2026-04-10 03:13:01 +0900

    Refactor blog structure: migrate blog posts from 'src/content/blog' to 'src/content/posts', update references and paths accordingly

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,9 +2,9 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-    // Load Markdown and MDX files in the `src/content/blog/` directory.
-    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const posts = defineCollection({
+    // Load Markdown and MDX files in the `src/content/posts/` directory.
+    loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
     // Type-check frontmatter using a schema
     schema: ({ image }) =>
         z.object({
@@ -17,4 +17,4 @@ const blog = defineCollection({
         }),
 });
 
-export const collections = { blog };
+export const collections = { posts };
