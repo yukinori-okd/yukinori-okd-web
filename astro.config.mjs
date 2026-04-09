edd43455f13e851f2d70be05beab24e4c3eb9887
Yukinori Okada <yukinori.okd@gmail.com>
2026-04-10 02:57:14 +0900

    Change the indentation from tabs to spaces

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,30 +6,30 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Atkinson',
+            cssVariable: '--font-atkinson',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/atkinson-regular.woff'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/atkinson-bold.woff'],
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
 });
