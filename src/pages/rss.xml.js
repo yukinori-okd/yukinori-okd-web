03b47a0be19d1350720992a85e0aa4a6476b80e3
Yukinori Okada <yukinori.okd@gmail.com>
2026-04-10 16:06:20 +0900

    Change site metadata: replace SITE_TITLE with MY_NAME across components and update page titles accordingly

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { SITE_DESCRIPTION, MY_NAME } from '../consts';
 
 export async function GET(context) {
     const posts = await getCollection('posts');
     return rss({
-        title: SITE_TITLE,
+        title: MY_NAME,
         description: SITE_DESCRIPTION,
         site: context.site,
         items: posts.map((post) => ({
