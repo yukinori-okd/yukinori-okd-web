c3be1555c146db0d44e2d3563fa6037b8ab2be94
Yukinori Okada <yukinori.okd@gmail.com>
2026-04-10 16:23:52 +0900

    Add tag feature

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const posts = defineCollection({
             pubDate: z.coerce.date(),
             updatedDate: z.coerce.date().optional(),
             heroImage: z.optional(image()),
+            tags: z.array(z.string()).optional(),
         }),
 });
 
