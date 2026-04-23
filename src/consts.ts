5b0826540ced063d01e8bc46c004083af4107007
Yukinori Okada <yukinori.okd@gmail.com>
2026-04-23 15:52:26 +0900

    Change the site description to one I think is appropriate

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const MY_NAME = 'Yukinori Okada';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const MY_NAME = "Yukinori Okada";
+export const SITE_DESCRIPTION = "Yukinori Okada's website!";
